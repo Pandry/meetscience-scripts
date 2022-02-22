f0eb0d1f90ab3e24fda20efd4f4609e70a8e79e0
Pandry <17499836+Pandry@users.noreply.github.com>
2022-02-22 02:38:36 +0100

    First working sample

--- aligner.lua
+++ aligner.lua
@@ -1,8 +1,7 @@
 obs = obslua
 enabled = false
-prefix = '^%d+x%d+_'
-padding = 300
-
+prefix = '^[vh]c_'
+padding = 40
 
 function ticker()
     if not enabled then
@@ -16,22 +15,16 @@ function ticker()
             local sourceKind = obs.obs_source_get_unversioned_id(source)
             if string.match(sourceName, prefix) ~= nil and sourceKind == "group" then
                 -- The scene matches the name
-                -- Get info about the rows and columns in the name
-                local rows = tonumber(string.match(sourceName,"%d+", 1))
-                local cols = tonumber(string.match(sourceName,"%d+", 2))
+                local isVerticalAlign = string.match(sourceName,"^v", 1) ~= nil 
 
-                if rows < 2 and cols < 2 then
-                    goto continue
-                end
-
-                --obs.obs_sceneitem_group_enum_items()
                 local group = obs.obs_group_from_source(source)
-                --if not obs.obs_sceneitem_is_group(source) then
-                --    print("Somehow we encountered a non-group")
-                --    goto continue
-                --end
                 local group_items = obs.obs_scene_enum_items(group)
                 
+                local firstIteration = true
+                local newScale = obs.vec2()
+                local i = 0
+                local height = 0
+                local width = 0
                 for _, group_item in ipairs(group_items) do
                     if not obs.obs_sceneitem_visible(group_item) then
                         goto continue_gil
@@ -41,20 +34,28 @@ function ticker()
                     local group_item_name = obs.obs_source_get_name(group_item_source)
 
                     
-                    print('found: ' .. group_item_name)
-                    local pos = obs.vec2() -- x,y
-                    obs.obs_sceneitem_get_pos(group_item, pos)
-                    local scale = obs.vec2() -- x,y
-                    obs.obs_sceneitem_get_scale(group_item, scale)
-                    print(scale.x ..','..scale.y)
+                    --print('found: ' .. group_item_name)
+                    --local pos = obs.vec2() -- x,y
+                    --obs.obs_sceneitem_get_pos(group_item, pos)
+
+                    if firstIteration then
+                        obs.obs_sceneitem_get_scale(group_item, newScale)
+                        height = round(obs.obs_source_get_height(group_item_source) * newScale.x)
+                        width = round(obs.obs_source_get_width(group_item_source) * newScale.y)
+                        --print('width='..width..'heigth='..height)
+                    else
+                        obs.obs_sceneitem_set_scale(group_item, newScale)
+                    end
+                    local pos = obs.vec2()
+                    if isVerticalAlign then
+                        obs.vec2_set(pos, 0, i*padding + i*height )
+                    else
+                        obs.vec2_set(pos, i*padding + i*width ,0)
+                    end
+                    obs.obs_sceneitem_set_pos(group_item, pos)
                     
-                    local swidth = obs.obs_source_get_width(group_item_source)
-                    local sheight = obs.obs_source_get_height(group_item_source)
-
-                    local width = swidth * scale.x
-                    local height = sheight * scale.y
-                    --print(width .. ','..height..','..scale)
-
+                    firstIteration = false
+                    i = i+1
                     ::continue_gil::
                 end
                 --obs.obs_scene_release(group) - no need for obs_group_from_source
@@ -70,6 +71,7 @@ function script_properties()
 	local props = obs.obs_properties_create()
 	obs.obs_properties_add_bool(props, "enabled", "Enabled")
     obs.obs_properties_add_text(props, "prefix", "Prefix of the group", obs.OBS_TEXT_DEFAULT)
+    obs.obs_properties_add_int(props, "padding", "Padding", 0, 1000, 1)
 
 	return props
 end
@@ -85,6 +87,7 @@ end
 function script_update(settings)
     enabled = obs.obs_data_get_bool(settings, "enabled")
     prefix  = obs.obs_data_get_string(settings, "prefix")
+    padding  = obs.obs_data_get_int(settings, "padding")
 end
 
 -- A function named script_defaults will be called to set the default settings
@@ -92,13 +95,18 @@ function script_defaults(settings)
 	--obs.obs_data_set_default_int(settings, "duration", 5)
 	obs.obs_data_set_default_string(settings, "prefix", prefix)
 	obs.obs_data_set_default_bool(settings, "enabled", enabled)
+	obs.obs_data_set_default_int(settings, "padding", padding)
 end
 
 -- a function named script_load will be called on startup
 function script_load(settings)
     -- Adds an timer callback which triggers every millseconds.
-    obs.timer_add(ticker, 4000)
+    obs.timer_add(ticker, 100)
 
     -- Removes a timer callback. (Note: You can also use remove_current_callback() to terminate the timer from the timer callback)
     -- timer_remove(callback)
 end
+
+function round(n)
+    return math.floor(n + 0.5)
+ end
